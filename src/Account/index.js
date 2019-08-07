d10a33d806e79422f4745137b3efe748dea49b78
Dongyuan Li <dongyuanli@Dongyuans-MacBook-Pro.local>
2019-08-07 17:32:04 -0600

    This is the HW

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,54 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+
+  constructor(){
+    super();
+    this.textInput = React.createRef();
+    this.state={
+      balance: 0,
+    }
+  };
+
+  increaseBalance = (e) => {
+    console.log(this.textInput.current.value);
+    this.setState({
+      balance: this.state.balance + parseInt(this.textInput.current.value) 
+    })
+  };
+
+  decreaseBalance = (e) => {
+    
+
+    let inputValue = parseInt(this.textInput.current.value)
+
+    if(this.state.balance - inputValue >= 0) {
+      this.setState({
+        balance: this.state.balance - inputValue
+      })
+    } else {
+      return false;      
+    }
+
+  //   if(this.state.balance >= 0) {
+  //   this.setState({
+  //     balance: this.state.balance - parseInt(this.textInput.current.value)
+  //   })
+  // } else {
+  //   this.setState({
+  //     banlance: 0
+  //   })
+  // }
+  };
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance ">${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={this.textInput} />
+        <input type="button" value="Deposit" onClick={this.increaseBalance}/>
+        <input type="button" value="Withdraw" onClick={this.decreaseBalance}/>
       </div>
     )
   }
